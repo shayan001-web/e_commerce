1064ccc95f5a580dab105e2a44bb05759f0afb77
shayan001-web <mshayanarain666@gmail.com>
2026-09-15 20:34:54 +0500

    Update api.js

--- ecommerce-project/frontend/src/services/api.js
+++ ecommerce-project/frontend/src/services/api.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
-const api = axios.create({ baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api' });
+const api = axios.create({ baseURL: import.meta.env.VITE_API_URL || 'https://e-commerce-backend1-m4hk2misu-shayan001-web.vercel.app/api' });
 api.interceptors.request.use(config => { const token = localStorage.getItem('nexa_token'); if (token) config.headers.Authorization = `Bearer ${token}`; return config; });
 export default api;
